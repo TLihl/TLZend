9ccf2164142e4862d690cf4b5a582aef3af1f922
TLihl <t.lihl@gmx.de>
2014-02-08 02:01:27 +0100

    Datenbank und Composer
    Hinzufügen der Datenbank und des Composers, um die Datenbank zu
    erstellen und das ZF2 in der richtigen Version mit dem Composer zu
    laden.

--- data/db/database.sql
+++ data/db/database.sql
@@ -5,3 +5,50 @@ GRANT USAGE ON * . * TO  'TLZend'@'localhost' IDENTIFIED BY  '***' WITH MAX_QUER
 CREATE DATABASE IF NOT EXISTS  `TLZend` ;
 
 GRANT ALL PRIVILEGES ON  `TLZend` . * TO  'TLZend'@'localhost';
+
+-- phpMyAdmin SQL Dump
+-- version 4.0.4.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 08, 2014 at 02:00 AM
+-- Server version: 5.5.32
+-- PHP Version: 5.4.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `tlzend`
+--
+CREATE DATABASE IF NOT EXISTS `tlzend` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `tlzend`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `article`
+--
+
+DROP TABLE IF EXISTS `article`;
+CREATE TABLE IF NOT EXISTS `article` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8_bin NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) COLLATE utf8_bin NOT NULL,
+  `password` varchar(100) COLLATE utf8_bin NOT NULL,
+  `update_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3 ;
